ec49a24a7fd7c7ad825fc66883354c396faf8540
Andres Correa <andrescorrea91@hotmail.com>
2018-09-19 07:43:48 -0700

    Se crea la interfaz IUsuariosRepository y la clase UsuariosRepository solo con las firmas de los metodos

---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/UsuariosRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Data.Entities;
+
+namespace MisMarcadores.Repository.Implementaciones
+{
+    public class UsuariosRepository : IUsuariosRepository
+    {
+        public bool Agregar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Borrar(string nombreUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Modificar(string nombreUsuario, Usuario nuevoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObtenerPorNombreUsuario(string nombreUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObtenerUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/IUsuariosRepository.cs
@@ -0,0 +1,18 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisMarcadores.Repository
+{
+    public interface IUsuariosRepository
+    {
+        IEnumerable<Usuario> ObtenerUsuarios();
+        Usuario ObtenerPorNombreUsuario(string nombreUsuario);
+        bool Agregar(Usuario usuario);
+        bool Borrar(string nombreUsuario);
+        bool Modificar(string nombreUsuario, Usuario nuevoUsuario);
+    }
+}
