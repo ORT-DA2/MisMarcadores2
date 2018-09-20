b227d4cd3971e5741135f37c2071ecd10414bb19
Andres Correa <andrescorrea91@hotmail.com>
2018-09-20 15:23:53 -0700

    Se crea la clase IRepository, con CRUD generico

---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MisMarcadores.Repository
+{
+    public interface IRepository<T>
+    {
+        void Agregar(T elemento);
+        T Obtener(Guid id);
+        void Borrar(Guid id);
+        void Modificar(T elemento);
+    }
+}
