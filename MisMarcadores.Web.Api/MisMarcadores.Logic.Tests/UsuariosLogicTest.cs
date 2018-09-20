727a4fd114efcbc79e89316e9db0def64d05209f
Andres Correa <andrescorrea91@hotmail.com>
2018-09-20 15:56:38 -0700

    Se crea la clase UsuariosLogicTest

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic.Tests/UsuariosLogicTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Logic;
+using MisMarcadores.Repository;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MisMarcadores.Logic.Tests
+{
+    [TestClass]
+    public class UsuariosLogicTest
+    {
+        
+    }
+}
