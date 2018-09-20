c307b1edb23cf28de10000999d739d53a669bbd2
Andres Correa <andrescorrea91@hotmail.com>
2018-09-20 15:21:14 -0700

    Se crea la clase MisMarcadoresContext dentro de DataAccess

---
+++ MisMarcadores.Web.Api/MisMarcadores.Data.DataAccess/MisMarcadoresContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MisMarcadores.Data.Entities;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class MisMarcadoresContext : DbContext
+    {
+        public MisMarcadoresContext(DbContextOptions<MisMarcadoresContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Equipo> Equipos { get; set; }
+        public DbSet<Deporte> Deportes { get; set; }
+        public DbSet<Encuentro> Encuentros { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Sesion> Sesiones { get; set; }
+    }
+
+}
