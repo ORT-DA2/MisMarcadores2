265476db957071539534f99cc7079f75bef25930
Andres Correa <andrescorrea91@hotmail.com>
2018-09-26 09:47:13 -0700

    Se modifica el Startup agregando las nuevas dependencias

--- MisMarcadores.Web.Api/MisMarcadores.Web.Api/Startup.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Startup.cs
@@ -4,17 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using MisMarcadores.Logic;
+using MisMarcadores.Web.Api.Models;
+using MisMarcadores.Repository;
+using MisMarcadores.Data.DataAccess;
 
 namespace MisMarcadores.Web.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            //Ejemplo de EF en Memoria
+            //services.AddDbContext<DomainContext>(options => options.UseInMemoryDatabase(Configuration.GetConnectionString("WACDatabase")));
+            //Ejemplo de EF con SQLServer
+            services.AddDbContext<MisMarcadoresContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MisMarcadoresDB")));
+            services.AddScoped<DbContext, MisMarcadoresContext>();
+
+            services.AddScoped<IUsuariosService, UsuariosService>();
+            services.AddScoped<IUsuariosRepository, UsuariosRepository>();
+
+            //SERVICES
+            services.AddScoped<IUsuariosService, UsuariosService>();
+            
+            //DATA ACCESS
+            services.AddScoped<IUsuariosRepository, UsuariosRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +57,7 @@ namespace MisMarcadores.Web.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
