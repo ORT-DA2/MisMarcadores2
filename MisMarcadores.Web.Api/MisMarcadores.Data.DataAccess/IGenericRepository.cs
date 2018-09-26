3ea910b0780ef18387c904b57d4ba9745aefe77d
Andres Correa <andrescorrea91@hotmail.com>
2018-09-26 09:30:07 -0700

    Se agregan las clases Genericas IGenericRepository y GenericRepository en DataAccess

---
+++ MisMarcadores.Web.Api/MisMarcadores.Data.DataAccess/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Web.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace MisMarcadores.Data.DataAccess
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        void Update(TEntity entityToUpdate);
+        IEnumerable<TEntity> GetAll();
+    }
+}
