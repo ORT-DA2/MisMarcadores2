1e10953de3f810f3b4a3121b329241d1f10d2f84
Andres Correa <andrescorrea91@hotmail.com>
2018-09-26 09:32:10 -0700

    Se implementa el patron UnitOfWork Se crean las clases UnitOfWork e IUnitOfWork

---
+++ MisMarcadores.Web.Api/MisMarcadores.Data.DataAccess/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Data.DataAccess
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Save();
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Data.DataAccess/UnitOfWork.cs
@@ -0,0 +1,124 @@
+﻿using MisMarcadores.Data.Entities;
+using MisMarcadores.Web.Api.Models;
+using System;
+
+namespace MisMarcadores.Data.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private MisMarcadoresContext context;
+        private GenericRepository<Deporte> deporteRepository;
+        private GenericRepository<Encuentro> encuentroRepository;
+        private GenericRepository<Equipo> equipoRepository;
+        private GenericRepository<Sesion> sesionRepository;
+        private GenericRepository<Usuario> usuarioRepository;
+        private GenericRepository<Comentario> comentarioRepository;
+
+        public UnitOfWork(MisMarcadoresContext context) {
+            this.context = context;
+        }
+
+        public GenericRepository<Deporte> DeporteRepository
+        {
+            get
+            {
+
+                if (this.deporteRepository == null)
+                {
+                    this.deporteRepository = new GenericRepository<Deporte>(context);
+                }
+                return deporteRepository;
+            }
+        }
+
+        public GenericRepository<Encuentro> EncuentroRepository
+        {
+            get
+            {
+
+                if (this.encuentroRepository == null)
+                {
+                    this.encuentroRepository = new GenericRepository<Encuentro>(context);
+                }
+                return encuentroRepository;
+            }
+        }
+
+        public GenericRepository<Sesion> SesionRepository
+        {
+            get
+            {
+
+                if (this.sesionRepository == null)
+                {
+                    this.sesionRepository = new GenericRepository<Sesion>(context);
+                }
+                return sesionRepository;
+            }
+        }
+
+        public GenericRepository<Equipo> EquipoRepository
+        {
+            get
+            {
+
+                if (this.equipoRepository == null)
+                {
+                    this.equipoRepository = new GenericRepository<Equipo>(context);
+                }
+                return equipoRepository;
+            }
+        }
+
+        public GenericRepository<Usuario> UsuarioRepository
+        {
+            get
+            {
+
+                if (this.usuarioRepository == null)
+                {
+                    this.usuarioRepository = new GenericRepository<Usuario>(context);
+                }
+                return usuarioRepository;
+            }
+        }
+
+        public GenericRepository<Comentario> ComentarioRepository
+        {
+            get
+            {
+
+                if (this.comentarioRepository == null)
+                {
+                    this.comentarioRepository = new GenericRepository<Comentario>(context);
+                }
+                return comentarioRepository;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
