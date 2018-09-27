2e473cddef56bd9c120899d77642a84fcf2a4aa6
Andres Correa <andrescorrea91@hotmail.com>
2018-09-27 14:36:23 -0700

    Se cambia el tipo de retorno del metodo ObtenerUsuarios

--- MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IUsuariosService.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IUsuariosService.cs
@@ -7,7 +7,7 @@ namespace MisMarcadores.Logic
     public interface IUsuariosService
     {
         void AgregarUsuario(Usuario usuario);
-        List<Usuario> ObtenerUsuarios();
+        IEnumerable<Usuario> ObtenerUsuarios();
         Usuario ObtenerPorNombreUsuario(string nombreUsuario);
     }
 }
