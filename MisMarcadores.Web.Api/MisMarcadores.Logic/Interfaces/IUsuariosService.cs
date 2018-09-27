88a16d2b093d77d4bc759c144e019c9c6f9d2f6b
Andres Correa <andrescorrea91@hotmail.com>
2018-09-27 09:38:23 -0700

    Se agregan metodos a la interfaz de la logica de Usuarios

--- MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IUsuariosService.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IUsuariosService.cs
@@ -1,10 +1,12 @@
 ﻿using MisMarcadores.Data.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace MisMarcadores.Logic
 {
     public interface IUsuariosService
     {
         void AgregarUsuario(Usuario usuario);
+        List<Usuario> ObtenerUsuarios();
     }
 }
