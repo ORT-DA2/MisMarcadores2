24a392dca572c66b520039f1a1859a85ef46fb75
Andres Correa <andrescorrea91@hotmail.com>
2018-09-28 14:05:14 -0700

    Se construyen los modelos que vamos a necesitar para el controlador de Usuarios

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/ActualizarUsuario.cs
@@ -0,0 +1,40 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class ActualizarUsuario
+    {
+        [Required]
+        public string Mail { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+        [Required]
+        public bool Administrador { get; set; }
+
+        public Usuario TransformarAUsuario()
+        {
+            return new Usuario
+            {
+                Nombre = this.Nombre,
+                Apellido = this.Apellido,
+                Mail = this.Mail,
+                Contraseña = this.Contraseña,
+                Administrador = this.Administrador
+            };
+        }
+
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/AgregarUsuario.cs
@@ -0,0 +1,41 @@
+﻿using MisMarcadores.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class AgregarUsuario
+    {
+        [Required]
+        public string Mail { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+        [Required]
+        public bool Administrador { get; set; }
+
+        public Usuario TransformarAUsuario()
+        {
+            return new Usuario
+            {
+                Nombre = this.Nombre,
+                Apellido = this.Apellido,
+                Mail = this.Mail,
+                NombreUsuario = this.NombreUsuario,
+                Contraseña = this.Contraseña,
+                Administrador = this.Administrador
+                
+            };
+        }
+
+    }
+}
