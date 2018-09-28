315fb3b59f6cc129344ffe9a571f4d9ccdfc6166
Andres Correa <andrescorrea91@hotmail.com>
2018-09-28 14:08:10 -0700

    Se implementa el repositorio de Usuarios con sus metodos definidos

--- MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/UsuariosRepository.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/UsuariosRepository.cs
@@ -1,8 +1,10 @@
 ﻿using MisMarcadores.Data.DataAccess;
 using MisMarcadores.Data.Entities;
+using MisMarcadores.Repository.Exceptions;
 using MisMarcadores.Web.Api.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MisMarcadores.Repository
@@ -10,5 +12,25 @@ namespace MisMarcadores.Repository
     public class UsuariosRepository : GenericRepository<Usuario>, IUsuariosRepository
     {
         public UsuariosRepository(MisMarcadoresContext context) : base(context) {}
+
+        public Usuario ObtenerPorNombreUsuario(string nombreUsuario)
+        {
+            return context.Usuarios.FirstOrDefault(x => x.NombreUsuario.Equals(nombreUsuario));
+        }
+
+        public void Modificar(Usuario usuario)
+        {
+            var usuarioOriginal = ObtenerPorNombreUsuario(usuario.NombreUsuario);
+            context.Usuarios.Attach(usuarioOriginal);
+            var entry = context.Entry(usuarioOriginal);
+            entry.CurrentValues.SetValues(usuario);
+        }
+
+        public void Borrar(Guid id) {
+            Usuario usuario = context.Usuarios.FirstOrDefault(u => u.Id == id);
+            context.Usuarios.Remove(usuario);
+        }
+
+
     }
 }
