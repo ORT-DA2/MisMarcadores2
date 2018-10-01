a7b40195b859e99fbe9ce8d05aabe07733e7a4dd
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 14:32:16 -0700

    Se implementa el metodo para que la prueba pase

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Implementaciones/SesionesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Data.DataAccess;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Repository;
+
+namespace MisMarcadores.Logic
+{
+    public class SesionesService : ISesionesService
+    {
+        IUnitOfWork _unitOfWork;
+        ISesionesRepository _sesionesRepository;
+
+        public SesionesService(IUnitOfWork unitOfWork, ISesionesRepository sesionesRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _sesionesRepository = sesionesRepository;
+        }
+
+        public Sesion Login(string nombreUsuario, string contraseña)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout(Guid token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sesion ObtenerPorToken(Guid token)
+        {
+            return _sesionesRepository.ObtenerPorToken(token);
+        }
+
+        public Usuario ObtenerUsuarioPorToken(Guid token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
