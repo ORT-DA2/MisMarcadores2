f9f97b7772f0ed7a4cba4d727c0b917b83cb0b7d
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 14:16:50 -0700

    Se crea la clase de prueba para la logica de Sesiones

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic.Tests/SesionesLogicTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Logic.Tests
+{
+    [TestClass]
+    public class SesionesLogicTest
+    {
+    }
+}
