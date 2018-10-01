6948dd2215608d6b504c1c52b1c71fcd39b70112
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 14:14:07 -0700

    Se crea la interfaz del repositorio de Sesiones, con la firma de sus metodos a implementar

---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/ISesionesRepository.cs
@@ -0,0 +1,16 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Repository
+{
+   public interface ISesionesRepository : IGenericRepository<Sesion>
+    {
+       Sesion ObtenerPorToken(Guid token);
+       void AgregarSesion(Sesion sesion);
+       void BorrarSesion(Guid token);
+       bool CredencialesValidas(string nombreUsuario, string contraseña);
+       Usuario ObtenerUsuarioPorToken(Guid token);
+    }
+}
