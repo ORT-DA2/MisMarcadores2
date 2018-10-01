99ae560802aa4b9e93e972910ae599499dc0172e
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 14:13:42 -0700

    Se crea la interfaz de la logica de Sesiones, con la firma de sus metodos a implementar

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/ISesionesService.cs
@@ -0,0 +1,13 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+
+namespace MisMarcadores.Logic
+{
+    public interface ISesionesService
+    {
+        Sesion ObtenerPorToken(Guid token);
+        Usuario ObtenerUsuarioPorToken(Guid token);
+        Sesion Login(string nombreUsuario, string contraseña);
+        void Logout(Guid token);
+    }
+}
