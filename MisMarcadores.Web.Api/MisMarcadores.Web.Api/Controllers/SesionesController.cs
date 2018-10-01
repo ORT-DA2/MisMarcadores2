b04c963c6a267a242d590f24e5a8af01add710b1
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 15:07:47 -0700

    Se implementa el controlador para las Sesiones

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Controllers/SesionesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Logic;
+using MisMarcadores.Web.Api.Filters;
+using MisMarcadores.Web.Api.Models;
+
+namespace MisMarcadores.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Sesiones")]
+    public class SesionesController : Controller
+    {
+        private ISesionesService _sesionesService { get; set; }
+
+        public SesionesController(ISesionesService sesionesService)
+        {
+            _sesionesService = sesionesService;
+        }
+
+        // GET: api/sesiones/nombreUsuario
+        [ServiceFilter(typeof(AutenticacionFilter))]
+        [HttpGet("{nombreUsuario}")]
+        public IActionResult Get(string nombreUsuario)
+        {
+            var headers = Request.Headers;
+            Guid token = new Guid(headers["tokenSesion"]);
+            Sesion sesion = _sesionesService.ObtenerPorToken(token);
+            if (sesion != null && sesion.NombreUsuario.Equals(nombreUsuario))
+            {
+                return Ok();
+            }
+            return Unauthorized();
+        }
+
+        // POST: api/sesiones
+        public IActionResult Post([FromBody]Login datosLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest("Datos invalidos");
+            try
+            {
+                Sesion sesionCreada = _sesionesService.Login(datosLogin.NombreUsuario, datosLogin.Contraseña);
+                return Ok(new { token = sesionCreada.Token });
+            }
+            catch (NullReferenceException)
+            {
+                return BadRequest("Usuario o contraseña incorrectos.");
+            }
+        }
+
+        // DELETE: api/sesiones/5
+        [ServiceFilter(typeof(AutenticacionFilter))]
+        [HttpDelete("{token}")]
+        public IActionResult Delete(Guid token)
+        {
+            var headers = Request.Headers;
+            string tokenStr = headers["tokenSesion"];
+            if (tokenStr.Equals(token.ToString()))
+            {
+                _sesionesService.Logout(token);
+                return Ok();
+            }
+            return Unauthorized();
+        }
+
+    }
+}
