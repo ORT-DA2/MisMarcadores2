c48819d65e5bf52fe6c18c1e120a975b7f51bb37
Andres Correa <andrescorrea91@hotmail.com>
2018-10-01 14:36:57 -0700

    Se crea nueva prueba en sesiones ObtenerSesionPorTokenErrorNotFoundTest

--- MisMarcadores.Web.Api/MisMarcadores.Logic.Tests/SesionesLogicTest.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Logic.Tests/SesionesLogicTest.cs
@@ -34,5 +34,27 @@ namespace MisMarcadores.Logic.Tests
             Assert.IsNotNull(obtainedResult);
             Assert.AreEqual(fakeSesion, obtainedResult);
         }
+
+        [TestMethod]
+        public void ObtenerSesionPorTokenErrorNotFoundTest()
+        {
+            //Arrange
+            var fakeSesion = TestHelper.ObtenerSesionFalsa();
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockSesionesRepository = new Mock<ISesionesRepository>();
+            mockSesionesRepository
+                .Setup(r => r.ObtenerPorToken(fakeSesion.Token))
+                .Returns((Sesion)null);
+
+            var businessLogic = new SesionesService(mockUnitOfWork.Object, mockSesionesRepository.Object);
+
+            //Act
+            Sesion obtainedResult = businessLogic.ObtenerPorToken(fakeSesion.Token);
+
+            //Assert
+            mockSesionesRepository.VerifyAll();
+            Assert.IsNull(obtainedResult);
+        }
     }
 }
