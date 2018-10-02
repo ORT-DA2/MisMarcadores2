f41721dc2da6fa0e597b9c7c5f1653d0fbc5771c
Andres Correa <andrescorrea91@hotmail.com>
2018-10-02 09:33:03 -0700

    Se implementa el repositorio de Deportes

--- MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/DeportesRepository.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/DeportesRepository.cs
@@ -2,6 +2,7 @@
 using MisMarcadores.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MisMarcadores.Repository
@@ -12,17 +13,21 @@ namespace MisMarcadores.Repository
 
         public void BorrarDeporte(string nombre)
         {
-            throw new NotImplementedException();
+            Deporte deporte = context.Deportes.FirstOrDefault(d => d.Nombre == nombre);
+            context.Deportes.Remove(deporte);
         }
 
         public void ModificarDeporte(Deporte deporte)
         {
-            throw new NotImplementedException();
+            var deporteOriginal = ObtenerDeportePorNombre(deporte.Nombre);
+            context.Deportes.Attach(deporteOriginal);
+            var entry = context.Entry(deporteOriginal);
+            entry.CurrentValues.SetValues(deporte);
         }
 
         public Deporte ObtenerDeportePorNombre(string nombre)
         {
-            throw new NotImplementedException();
+            return context.Deportes.FirstOrDefault(x => x.Nombre.Equals(nombre));
         }
     }
 }
