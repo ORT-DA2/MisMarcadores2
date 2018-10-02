b4b46bc105c8f435a5b4d7a98b883f9f37dce96c
Andres Correa <andrescorrea91@hotmail.com>
2018-10-02 13:35:48 -0700

    Se crea el controlador de Deportes con sus metodos

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Controllers/DeportesController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Logic;
+using MisMarcadores.Repository.Exceptions;
+using MisMarcadores.Web.Api.Filters;
+using MisMarcadores.Web.Api.Models;
+
+namespace MisMarcadores.Web.Api
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+
+    [ServiceFilter(typeof(AutenticacionFilter))]
+    public class DeportesController : Controller
+    {
+        private IDeportesService _deportesService { get; set; }
+
+        public DeportesController(IDeportesService deportesService)
+        {
+            _deportesService = deportesService;
+        }
+
+        // GET: api/Deportes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Deporte> deportes = _deportesService.ObtenerDeportes();
+            if (deportes == null)
+            {
+                return NotFound();
+            }
+            return Ok(deportes);
+        }
+
+        // GET: api/Deportes
+        [HttpGet("{nombreDeporte}", Name = "GetDeporte")]
+        public IActionResult Get(string nombreDeporte)
+        {
+            Deporte deporte = _deportesService.ObtenerDeportePorNombre(nombreDeporte);
+            if (deporte == null)
+            {
+                return NotFound();
+            }
+            return Ok(deporte);
+        }
+
+        // POST: api/deportes
+        public IActionResult Post([FromBody]AgregarDeporte deporte)
+        {
+            if (!ModelState.IsValid) return BadRequest("Datos invalidos");
+            try
+            {
+                this._deportesService.AgregarDeporte(deporte.TransformarADeporte());
+                return CreatedAtRoute("GetDeporte", new { nombreDeporte = deporte.Nombre }, deporte);
+            }
+            catch (DeporteDataException)
+            {
+                return BadRequest("Datos invalidos");
+            }
+            catch (ExisteDeporteException)
+            {
+                return StatusCode(409, "El nombre del deporte ya existe en la BD.");
+            }
+        }
+
+        // PUT: api/Deportes/Futbol
+        [HttpPut("{nombreDeporte}")]
+        public IActionResult Put(string nombreDeporte, [FromBody]ActualizarDeporte deporte)
+        {
+            if (!ModelState.IsValid) return BadRequest("Datos invalidos");
+            try
+            {
+                this._deportesService.ModificarDeporte(nombreDeporte, deporte.TransformarADeporte());
+                return Ok();
+            }
+            catch (DeporteDataException)
+            {
+                return BadRequest("Datos invalidos");
+            }
+            catch (NoExisteDeporteException)
+            {
+                return BadRequest("El deporte no existe en la BD.");
+            }
+            catch (ExisteDeporteException)
+            {
+                return StatusCode(409, "El nuevo nombre del deporte ya existe en la BD.");
+            }
+        }
+
+        // DELETE: api/Usuarios/Futbol
+        [HttpDelete("{nombreDeporte}")]
+        public IActionResult Delete(string nombreDeporte)
+        {
+            try
+            {
+                this._deportesService.BorrarDeporte(nombreDeporte);
+                return Ok();
+            }
+            catch (NoExisteDeporteException)
+            {
+                return BadRequest("El deporte no existe en la BD.");
+            }
+            catch (RepositoryException)
+            {
+                return BadRequest("El deporte no existe en la BD.");
+            }
+        }
+    }
+}
