4cc91510ed909a7efc92ea23cc6e447dd4185a0e
Andres Correa <andrescorrea91@hotmail.com>
2018-10-02 06:55:10 -0700

    Se crea la clase de la logica de deportes con su constructor y sus metodos no implementados

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Implementaciones/DeportesService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Data.DataAccess;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Repository;
+
+namespace MisMarcadores.Logic
+{
+    public class DeportesService : IDeportesService
+    {
+        IUnitOfWork _unitOfWork;
+        IDeportesRepository _deportesRepository;
+
+        public DeportesService(IUnitOfWork unitOfWork, IDeportesRepository deportesRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _deportesRepository = deportesRepository;
+        }
+
+        public void AgregarDeporte(Deporte deporte)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BorrarDeporte(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModificarDeporte(string nombre, Deporte deporte)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deporte ObtenerDeportePorNombre(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Deporte> ObtenerDeportes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
