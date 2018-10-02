0981cbd17cdc1603e1ca1af0a3bc7c858f4c29fb
Andres Correa <andrescorrea91@hotmail.com>
2018-10-02 13:35:29 -0700

    Se crean los modelos necesarios para la web api de Deportes

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/ActualizarDeporte.cs
@@ -0,0 +1,19 @@
+﻿using MisMarcadores.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class ActualizarDeporte
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        public Deporte TransformarADeporte()
+        {
+            return new Deporte
+            {
+                Nombre = this.Nombre
+            };
+        }
+    }
+}
