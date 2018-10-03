9a2955b5a51d92405d531f97cb2b4d9dfac8f3ce
Andres Correa <andrescorrea91@hotmail.com>
2018-10-03 14:49:39 -0700

    Se crean las interfaces para encuentros con sus firmas, para la logica y el repositorio

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IEncuentrosService.cs
@@ -0,0 +1,16 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Logic
+{
+    public interface IEncuentrosService
+    {
+        Guid AgregarEncuentro(Encuentro encuentro);
+        IEnumerable<Encuentro> ObtenerEncuentros();
+        IEnumerable<Encuentro> ObtenerEncuentrosDeEquipo(Guid id);
+        void ModificarEncuentro(Guid id, Encuentro encuentro);
+        void BorrarEncuentro(Guid id);
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/IEncuentrosRepository.cs
@@ -0,0 +1,15 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Repository
+{
+    public interface IEncuentrosRepository : IGenericRepository<Encuentro>
+    {
+        Encuentro ObtenerEncuentroPorId(Guid id);
+        void ModificarEncuentro(Encuentro encuentro);
+        void BorrarEncuentro(Guid id);
+        List<Encuentro> ObtenerEncuentros();
+    }
+}
