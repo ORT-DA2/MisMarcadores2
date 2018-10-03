73333ad04cdb1ed77ba514ffbfb187f2acec7dfc
Andres Correa <andrescorrea91@hotmail.com>
2018-10-03 12:01:09 -0700

    Se crean los modelos necesarios para el controlador de Equipos

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/ActualizarEquipo.cs
@@ -0,0 +1,23 @@
+﻿using MisMarcadores.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class ActualizarEquipo
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string Foto { get; set; }
+
+        public Equipo TransformarAEquipo()
+        {
+            return new Equipo
+            {
+                Nombre = this.Nombre,
+                Foto = this.Foto,
+            };
+        }
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/AgregarEquipo.cs
@@ -0,0 +1,27 @@
+﻿using MisMarcadores.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class AgregarEquipo
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string Foto { get; set; }
+
+        [Required]
+        public string NombreDeporte { get; set; }
+
+        public Equipo TransformarAEquipo()
+        {
+            return new Equipo
+            {
+                Nombre = this.Nombre,
+                Foto = this.Foto,
+                Deporte = new Deporte { Nombre = this.NombreDeporte }
+            };
+        }
+    }
+}
