4d9c6edd425c58e2a987b8e9388ee6e83f2fcea1
Andres Correa <andrescorrea91@hotmail.com>
2018-10-03 06:01:22 -0700

    Se crean las interfaces para equipos con sus firmas, para la logica y el repositorio

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IEquiposService.cs
@@ -0,0 +1,15 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MisMarcadores.Logic
+{
+    public interface IEquiposService
+    {
+        void AgregarEquipo(Equipo equipo);
+        IEnumerable<Equipo> ObtenerEquipos();
+        Equipo ObtenerEquipoPorNombre(String nombre);
+        void ModificarEquipo(String nombre, Equipo equipo);
+        void BorrarEquipo(String nombre);
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/IEquiposRepository.cs
@@ -0,0 +1,14 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Repository
+{
+    public interface IEquiposRepository : IGenericRepository<Equipo>
+    {
+        Deporte ObtenerEquipoPorNombre(String nombre);
+        void ModificarEquipo(Equipo equipo);
+        void BorrarEquipo(String nombre);
+    }
+}
