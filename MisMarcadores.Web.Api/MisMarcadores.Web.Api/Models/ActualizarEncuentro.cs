70ce390fd2ea70415be1cc9be29316b2ef902ab1
Andres Correa <andrescorrea91@hotmail.com>
2018-10-05 06:46:09 -0700

    Se crean los modelos necesarios para el controller de Encuentros

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/ActualizarEncuentro.cs
@@ -0,0 +1,32 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class ActualizarEncuentro
+    {
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public string EquipoLocal { get; set; }
+
+        [Required]
+        public string EquipoVisitante { get; set; }
+
+        [Required]
+        public string NombreDeporte { get; set; }
+
+        public Encuentro TransformarAEncuentro()
+        {
+            return new Encuentro
+            {
+                FechaHora = this.Fecha,
+                EquipoLocal = new Equipo { Nombre = this.EquipoLocal },
+                EquipoVisitante = new Equipo { Nombre = this.EquipoVisitante },
+                Deporte = new Deporte { Nombre = this.NombreDeporte }
+            };
+        }
+    }
+}
