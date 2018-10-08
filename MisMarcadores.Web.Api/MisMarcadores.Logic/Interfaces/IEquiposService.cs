6f5364848ae41f254b47d1aef193457f32d9c6d1
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 11:28:05 -0700

    Se crean nuevos metodos en la interfaz de la logica de equipos, los necesarios para la logica de favoritos

--- MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IEquiposService.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IEquiposService.cs
@@ -7,8 +7,11 @@ namespace MisMarcadores.Logic
     public interface IEquiposService
     {
         Guid AgregarEquipo(Equipo equipo);
+        void AgregarFavorito(Guid idEquipo, String nombreUsuario);
+        void BorrarFavorito(Guid idEquipo, String nombreUsuario);
         IEnumerable<Equipo> ObtenerEquipos();
         IEnumerable<Equipo> ObtenerEquiposPorDeporte(String deporte);
+        IEnumerable<Guid> ObtenerFavoritosPorUsuario(String nombreUsuario);
         Equipo ObtenerEquipoPorId(Guid id);
         Equipo ObtenerEquipoPorNombre(String nombre);
         void ModificarEquipo(Guid id, Equipo equipo);
