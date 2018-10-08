3c0a2b89637093c8e3e61afa1bd56a6cc28af251
Andres Correa <andrescorrea91@hotmail.com>
2018-10-07 18:49:27 -0700

    Se crea una implementacion de IFixture, creamos e implementamos la clase FixtureLiga

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Implementaciones/FixtureLiga.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Data.Entities;
+
+namespace MisMarcadores.Logic
+{
+    public class FixtureLiga : IFixture
+    {
+        private DateTime fechaInicio;
+        private List<Equipo> equipos;
+        
+        public FixtureLiga(DateTime fechaInicio, List<Equipo> equipos)
+        {
+            this.fechaInicio = fechaInicio;
+            this.equipos = equipos;
+        }
+
+        public List<Encuentro> GenerarFixture()
+        {
+            List<Encuentro> encuentros = new List<Encuentro>();
+            int fechaEncuentro = 0;
+            for (int i = 0; i < equipos.Count; i++)
+            {
+                for (int j = 0; j < equipos.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        Encuentro encuentro = new Encuentro
+                        {
+                            EquipoLocal = equipos[i],
+                            EquipoVisitante = equipos[j],
+                            FechaHora = fechaInicio.AddDays(fechaEncuentro)
+                        };
+                        encuentros.Add(encuentro);
+                        fechaEncuentro += 3;
+                    }
+                }
+            }
+            return encuentros;
+        }
+    }
+}
