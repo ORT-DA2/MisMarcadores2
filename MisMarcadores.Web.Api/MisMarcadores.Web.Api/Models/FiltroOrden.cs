f388a2fb59d24fff1e4eb13b437bbfda79cdd1a7
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 16:34:50 -0700

    Se crea un modelo para el filtro y el orden de los equipos

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/FiltroOrden.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class FiltroOrden
+    {
+        public string Filtro { get; set; }
+
+        public string Orden { get; set; }
+
+    }
+}
