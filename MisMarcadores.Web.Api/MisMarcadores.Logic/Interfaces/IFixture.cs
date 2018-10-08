e3d48e7231e967f048435c58ad02a10be885adb7
Andres Correa <andrescorrea91@hotmail.com>
2018-10-07 18:46:31 -0700

    Se crea la interfaz IFixture, con el metodo GenerarFixture

---
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Interfaces/IFixture.cs
@@ -0,0 +1,12 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Logic
+{
+    public interface IFixture
+    {
+        List<Encuentro> GenerarFixture();
+    }
+}
