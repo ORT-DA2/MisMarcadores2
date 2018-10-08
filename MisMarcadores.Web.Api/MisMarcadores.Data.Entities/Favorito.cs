9c80d93a44db8585c3feebc96b9fa90e7b70ac19
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 11:25:07 -0700

    Se crea la entidad Favorito

---
+++ MisMarcadores.Web.Api/MisMarcadores.Data.Entities/Favorito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MisMarcadores.Data.Entities
+{
+    public class Favorito
+    {
+        public Guid Id { get; set; }
+        public String NombreUsuario { get; set; }
+        public Guid IdEquipo { get; set; }
+
+        public Favorito()
+        {
+
+        }
+    }
+}
+
