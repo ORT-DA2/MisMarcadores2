d98b7aee37ac3c71b8c93154c742b4c630c28554
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 11:24:49 -0700

    Se modifica la entidad Comentario, en lugar de tener un usuario y un encuentro, solo tiene los ids que los referencian

--- MisMarcadores.Web.Api/MisMarcadores.Data.Entities/Comentario.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Data.Entities/Comentario.cs
@@ -5,13 +5,12 @@ namespace MisMarcadores.Data.Entities
     public class Comentario
     {
         public Guid Id { get; set; }
-        public Usuario Usuario { get; set; }
-        public Encuentro Encuentro { get; set; }
+        public String NombreUsuario { get; set; }
+        public Guid IdEncuentro { get; set; }
         public String Texto { get; set; }
 
         public Comentario() {
-            Usuario = new Usuario();
-            Encuentro = new Encuentro();
+           
         }
     }
 }
