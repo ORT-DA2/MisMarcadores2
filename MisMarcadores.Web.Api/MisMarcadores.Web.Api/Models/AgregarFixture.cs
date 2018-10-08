da3ab67c52f239221cf03757245f60403107e448
Andres Correa <andrescorrea91@hotmail.com>
2018-10-07 18:47:48 -0700

    Se crea el modelo necesario para la creacion de un fixture

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/AgregarFixture.cs
@@ -0,0 +1,19 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class AgregarFixture
+    {
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        public DateTime FechaInicio { get; set; }
+
+        [Required]
+        public string Deporte { get; set; }
+
+    }
+}
