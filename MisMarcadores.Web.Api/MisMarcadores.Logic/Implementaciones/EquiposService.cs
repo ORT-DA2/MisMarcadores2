eab5df8df78dd83b8d707b9b2f96bbedd0eed862
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 12:07:25 -0700

    Se refactorea la logica de Equipos para contemplar los favoritos

--- MisMarcadores.Web.Api/MisMarcadores.Logic/Implementaciones/EquiposService.cs
+++ MisMarcadores.Web.Api/MisMarcadores.Logic/Implementaciones/EquiposService.cs
@@ -12,12 +12,15 @@ namespace MisMarcadores.Logic
         private IUnitOfWork _unitOfWork;
         private IEquiposRepository _equiposRepository;
         private IDeportesRepository _deportesRepository;
+        private IFavoritosRepository _favoritosRepository;
 
-        public EquiposService(IUnitOfWork unitOfWork, IEquiposRepository equiposRepository, IDeportesRepository deportesRepository)
+        public EquiposService(IUnitOfWork unitOfWork, IEquiposRepository equiposRepository, 
+            IDeportesRepository deportesRepository, IFavoritosRepository favoritosRepository)
         {
             _unitOfWork = unitOfWork;
             _equiposRepository = equiposRepository;
             _deportesRepository = deportesRepository;
+            _favoritosRepository = favoritosRepository;
         }
 
         public Guid AgregarEquipo(Equipo equipo)
@@ -39,6 +42,48 @@ namespace MisMarcadores.Logic
                 _equiposRepository.Insert(equipo);
                 _unitOfWork.Save();
                 return equipo.Id;
+            }
+            catch (RepositoryException)
+            {
+                throw new RepositoryException();
+            }
+        }
+
+        public void AgregarFavorito(Guid idEquipo, string nombreUsuario)
+        {
+            Equipo equipo = ObtenerEquipoPorId(idEquipo);
+            if (equipo == null)
+                throw new NoExisteEquipoException();
+            if (_favoritosRepository.ExisteFavorito(nombreUsuario, idEquipo))
+                throw new ExisteFavoritoException();
+
+            try
+            {
+                Favorito favorito = new Favorito
+                {
+                    IdEquipo = idEquipo,
+                    NombreUsuario = nombreUsuario
+                };
+                _favoritosRepository.Insert(favorito);
+                _unitOfWork.Save();
+            }
+            catch (RepositoryException)
+            {
+                throw new RepositoryException();
+            }
+        }
+
+        public void BorrarFavorito(Guid idEquipo, string nombreUsuario)
+        {
+            Equipo equipo = ObtenerEquipoPorId(idEquipo);
+            if (equipo == null)
+                throw new NoExisteEquipoException();
+            if (!_favoritosRepository.ExisteFavorito(nombreUsuario, idEquipo))
+                throw new NoExisteFavoritoException();
+            try
+            {
+                _favoritosRepository.BorrarFavorito(nombreUsuario, idEquipo);
+                _unitOfWork.Save();
             }
             catch (RepositoryException)
             {
@@ -103,6 +148,18 @@ namespace MisMarcadores.Logic
         public IEnumerable<Equipo> ObtenerEquiposPorDeporte(string deporte)
         {
             return _equiposRepository.ObtenerEquiposPorDeporte(deporte);
+        }
+
+        public IEnumerable<Guid> ObtenerFavoritosPorUsuario(string nombreUsuario)
+        {
+            List<Guid> idEquipos = new List<Guid>();
+            List<Favorito> favoritos = _favoritosRepository.ObtenerFavoritosPorUsuario(nombreUsuario);
+            foreach (Favorito favorito in favoritos)
+            {
+                if (favorito.NombreUsuario.Equals(nombreUsuario))
+                    idEquipos.Add(favorito.IdEquipo);
+            }
+            return idEquipos;
         }
 
         private bool CampoValido(string campo)
