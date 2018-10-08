7403a42aa84406cc094431c8fe3f11b2eb4aa159
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 12:17:29 -0700

    Se crea nuevo modelo para poder agregar un comentario llamado AgregarComentario

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/AgregarComentario.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class AgregarComentario
+    {
+        [Required]
+        public string Comentario { get; set; }
+    }
+}
