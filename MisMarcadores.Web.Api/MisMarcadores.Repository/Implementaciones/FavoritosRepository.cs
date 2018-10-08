10b33de539bbd57119da6f90267b831b62901d81
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 11:29:38 -0700

    Se crea e implementa el repositorio para los favoritos

---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Implementaciones/FavoritosRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MisMarcadores.Data.DataAccess;
+using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MisMarcadores.Repository
+{
+    public class FavoritosRepository : GenericRepository<Favorito>, IFavoritosRepository
+    {
+        public FavoritosRepository(MisMarcadoresContext context) : base(context) { }
+
+        public void BorrarFavorito(string nombreUsuario, Guid idEquipo)
+        {
+            Favorito favorito = context.Favoritos.FirstOrDefault(f => f.IdEquipo == idEquipo && f.NombreUsuario == nombreUsuario);
+            context.Favoritos.Remove(favorito);
+        }
+
+        public bool ExisteFavorito(string nombreUsuario, Guid idEquipo)
+        {
+            return context.Favoritos.Any(f => f.NombreUsuario == nombreUsuario && f.IdEquipo == idEquipo);
+        }
+
+        public List<Favorito> ObtenerFavoritosPorUsuario(string nombreUsuario)
+        {
+            return context.Favoritos.Where(x => x.NombreUsuario.Equals(nombreUsuario)).ToList();
+        }
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Repository/Interfaces/IFavoritosRepository.cs
@@ -0,0 +1,14 @@
+﻿using MisMarcadores.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MisMarcadores.Repository
+{
+    public interface IFavoritosRepository : IGenericRepository<Favorito>
+    {
+        void BorrarFavorito(String nombreUsuario, Guid idEquipo);
+        bool ExisteFavorito(String nombreUsuario, Guid idEquipo);
+        List<Favorito> ObtenerFavoritosPorUsuario(String nombreUsuario);
+    }
+}
