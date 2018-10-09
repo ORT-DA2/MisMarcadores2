a3c0a0e1d3e40ab7ee1ea1bed3ae912553845937
Andres Correa <andrescorrea91@hotmail.com>
2018-10-08 17:36:58 -0700

    Se crea el modelo para obtener un calendario, con mes y ano

---
+++ MisMarcadores.Web.Api/MisMarcadores.Web.Api/Models/Calendario.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MisMarcadores.Web.Api.Models
+{
+    public class Calendario
+    {
+        public int Mes { get; set; }
+
+        public int Año { get; set; }
+    }
+}
