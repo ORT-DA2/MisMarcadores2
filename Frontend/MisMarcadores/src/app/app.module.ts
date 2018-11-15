455634ca7a36b95a3bc368a4431da9b6eb258a83
Andres Correa <andresco@oktana.io>
2018-11-15 16:20:27 -0300

    Se modifica el module de la app agregando todos los componentes creados

--- Frontend/MisMarcadores/src/app/app.module.ts
+++ Frontend/MisMarcadores/src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SessionService } from './servicios/session.service';
+import { HomeComponent } from './home/home.component';
+import { UserService } from './servicios/usuario.service';
+import { BaseApiService } from './servicios/base-api.service';
+import { CoreModule } from './core/core.module';
+
+const appRoutes: Routes = [
+  {
+    path: '', component: HomeComponent,
+    children: [
+      { path: 'home', component: HomeComponent }
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    CoreModule,
   ],
-  providers: [],
+  providers: [
+    SessionService,
+    UserService,
+    BaseApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
