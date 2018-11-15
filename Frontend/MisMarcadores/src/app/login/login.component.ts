201a2ae3d5e8441f0200832680d644d693fea1ed
Andres Correa <andresco@oktana.io>
2018-11-15 16:13:39 -0300

    Se crea un componente para el login

---
+++ Frontend/MisMarcadores/src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../service/session.service';
+import { UserService } from '../service/user.service';
+import { LoginRequest } from '../interfaces/login-request';
+import { Session } from '../interfaces/session';
+import { Router } from '@angular/router';
+import { NotificationService } from '../core/notification.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model: LoginRequest;
+  submitted: boolean;
+
+  constructor(
+    private sessionService: SessionService,
+    private userService: UserService,
+    private router: Router,
+    private notificationService: NotificationService) {
+    this.model = { nombreUsuario: '', contrasena: '' };
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.userService.postLogin(this.model).subscribe((response: Session) => {
+      if (response) {
+        this.notificationService.display({ message: 'Usuario Logueado!', severity: 'info' });
+
+        this.sessionService.setSession(response);
+
+        if (this.sessionService.isAuthenticated) {
+          this.router.navigate([this.sessionService.attemptedUrl]);
+        }
+      } else {
+        this.notificationService.display({ message: 'Invalid username or password', severity: 'error' });
+      }
+
+    }, () => {
+      this.notificationService.display({ message: 'Error occurred', severity: 'error' });
+    });
+  }
+
+}
