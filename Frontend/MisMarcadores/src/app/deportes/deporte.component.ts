59f173349b0bff0db474e48670fff1c25ff823dc
Andres Correa <andresco@oktana.io>
2018-11-15 18:08:37 -0300

    Se crea un componente para deportes

---
+++ Frontend/MisMarcadores/src/app/deportes/deporte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deportes',
+  templateUrl: './deporte.component.html',
+  styleUrls: ['./deporte.component.css']
+})
+export class DeporteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
