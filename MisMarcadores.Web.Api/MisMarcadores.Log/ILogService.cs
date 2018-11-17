38db87090ba42b555bb14e3a730aa3975fb0a737
Marcelo <marcelogurban@gmail.com>
2018-11-16 22:19:55 -0300

    Se incluye el Proyecto Log

---
+++ MisMarcadores.Web.Api/MisMarcadores.Log/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MisMarcadores.Data.Entities;
+
+namespace MisMarcadores.Log
+{
+    public interface ILogService
+    {
+        void InsertarAccion(string nombreUsuario);
+        IEnumerable<MisMarcadores.Data.Entities.Log> ObtenerLog();
+        List<MisMarcadores.Data.Entities.Log> ObtenerLogEntreFechas(FilterDateTime filter);
+    }
+}
---
+++ MisMarcadores.Web.Api/MisMarcadores.Log/LogService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MisMarcadores.Data.DataAccess;
+using MisMarcadores.Data.Entities;
+using MisMarcadores.Repository;
+
+
+namespace MisMarcadores.Log
+{
+    public class LogService : ILogService
+    {
+
+        private IUnitOfWork _unitOfWork;
+        private ILogRepository _logRepository;
+
+        public LogService(IUnitOfWork unitOfWork, ILogRepository logRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _logRepository = logRepository;
+            
+        }
+        public IEnumerable<MisMarcadores.Data.Entities.Log> ObtenerLog()
+        {          
+                return _logRepository.GetAll();           
+        }
+
+        public List<MisMarcadores.Data.Entities.Log> ObtenerLogEntreFechas(FilterDateTime filter)
+        {
+            IEnumerable<MisMarcadores.Data.Entities.Log> logs = ObtenerLog();
+            List<Data.Entities.Log> LogsEnFecha = new List<Data.Entities.Log>();
+            foreach (MisMarcadores.Data.Entities.Log log in logs)
+            {
+                int esDespuesFechaInicio = DateTime.Compare(filter.Inicio, log.Fecha);
+                int esAntesFechaFin = DateTime.Compare( log.Fecha, filter.Fin);
+                if (esDespuesFechaInicio == -1 && esAntesFechaFin == -1)
+                    LogsEnFecha.Add(log);       
+            }
+            return LogsEnFecha;
+        }
+
+        public void InsertarAccion(string nombreUsuario)
+        {
+            MisMarcadores.Data.Entities.Log aInsertar = new MisMarcadores.Data.Entities.Log();
+            aInsertar.Accion = "Login";
+            aInsertar.Fecha = DateTime.Now;
+            aInsertar.Usuario = nombreUsuario;
+            _logRepository.Insert(aInsertar);
+            _unitOfWork.Save();
+        }
+
+
+    }
+
+    public class FilterDateTime
+    {
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+    }
+}
