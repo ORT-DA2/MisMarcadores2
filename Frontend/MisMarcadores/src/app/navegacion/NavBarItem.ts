0e251a2bba340451f6918c60ba4e5e16595716c7
Andres Correa <andrescorrea91@hotmail.com>
2018-11-18 10:37:41 -0800

    Se agrega un componente para la navegacion

---
+++ Frontend/MisMarcadores/src/app/navegacion/NavBarItem.ts
@@ -0,0 +1,4 @@
+export class NavBarItem {
+  name: string;
+  uri: string;
+}
---
+++ Frontend/MisMarcadores/src/app/navegacion/navegacion.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavBarItem } from './NavBarItem';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SesionService } from '../servicios/sesion.service';
+import { UsuarioService } from '../servicios/usuario.service';
+
+@Component({
+  selector: 'app-navegacion',
+  templateUrl: './navegacion.component.html',
+  styleUrls: ['./navegacion.component.css']
+})
+
+export class NavegacionComponent implements OnInit {
+  public uri = '';
+  public nombreUsuario = '';
+
+  public items: NavBarItem[] = [
+    { name: 'Inicio', uri: 'home' },
+    { name: 'Favoritos', uri: 'favoritos' }
+  ];
+
+  constructor(
+    private router: Router,
+    private a: ActivatedRoute,
+    private auth: SesionService,
+    private self: UsuarioService
+  ) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      this.uri = location.pathname.split('/')[1];
+    });
+    this.self.getUsuario().subscribe(
+      data => {
+        console.log(data);
+        this.nombreUsuario = data.nombreUsuario;
+        
+        if(data.administrador){
+          this.loadAdminMenu();
+        }
+      }
+    );
+  }
+
+  private loadAdminMenu(): void {
+    this.items.push({ name: 'Usuarios', uri: 'usuarios' });
+    this.items.push({ name: 'Fixtures', uri: 'fixture' });
+  }
+
+  public logout(): void {
+    this.auth.logOff();
+    this.router.navigate(['login']);
+  }
+}
