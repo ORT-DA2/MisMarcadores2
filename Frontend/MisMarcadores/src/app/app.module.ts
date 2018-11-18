dad8340f5c6edbb24581ca70040e33ee58329aac
Andres Correa <andrescorrea91@hotmail.com>
2018-11-18 10:38:49 -0800

    Se modifica el modulo principal de la aplicacion

--- Frontend/MisMarcadores/src/app/app.module.ts
+++ Frontend/MisMarcadores/src/app/app.module.ts
@@ -1,27 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TokenGuard } from './token-guard';
 
 import { RouterModule, Routes } from '@angular/router';
 
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SessionService } from './servicios/session.service';
-import { HomeComponent } from './home/home.component';
+import { SesionService } from './servicios/sesion.service';
+import { HomeModule } from './home/home.component';
 import { DeporteComponent } from './deportes/deporte.component';
-import { UserService } from './servicios/usuario.service';
+import { UsuarioService } from './servicios/usuario.service';
 import { BaseApiService } from './servicios/base-api.service';
 import { CoreModule } from './core/core.module';
 
 const appRoutes: Routes = [
-  {
-    path: '', component: HomeComponent,
-    children: [
-      { path: 'home', component: HomeComponent }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'deportes', component: DeporteComponent },
   { path: '**', component: PageNotFoundComponent }
@@ -31,9 +28,10 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
     DeporteComponent,
     PageNotFoundComponent,
+    MenuBarComponent,
+    NavegacionComponent,
   ],
   imports: [
     RouterModule.forRoot(
@@ -44,10 +42,12 @@ const appRoutes: Routes = [
     FormsModule,
     HttpClientModule,
     CoreModule,
+    HomeModule
   ],
   providers: [
-    SessionService,
-    UserService,
+    TokenGuard,
+    SesionService,
+    UsuarioService,
     BaseApiService
   ],
   bootstrap: [AppComponent]
