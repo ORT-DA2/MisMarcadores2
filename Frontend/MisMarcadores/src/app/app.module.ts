2fca5bd8db9de3df8f8483fe31854870895a329b
Andres Correa <andresco@oktana.io>
2018-11-19 15:34:46 -0300

    Se agregan las dependencias en la app

--- Frontend/MisMarcadores/src/app/app.module.ts
+++ Frontend/MisMarcadores/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TokenGuard } from './token-guard';
+import icons from 'glyphicons';
 
 import { RouterModule, Routes } from '@angular/router';
 
@@ -10,18 +11,29 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { UsuariosListComponent } from './usuarios/usuarios-list.component';
+import { DeporteComponent } from './deportes/deporte.component';
+import { DeportesListComponent } from './deportes/deportes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SesionService } from './servicios/sesion.service';
 import { HomeModule } from './home/home.component';
-import { DeporteComponent } from './deportes/deporte.component';
+import { YesNoPipe } from './pipes/yesNo.pipe';
 import { UsuarioService } from './servicios/usuario.service';
+import { DeporteService } from './servicios/deporte.service';
 import { BaseApiService } from './servicios/base-api.service';
 import { CoreModule } from './core/core.module';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'deportes', component: DeporteComponent },
+  { path: 'usuarios', component: UsuariosListComponent, canActivate: [TokenGuard] },
+  { path: 'usuarios/agregar', component: UsuariosComponent, canActivate: [TokenGuard] },
+  { path: 'usuarios/:nombreUsuario', component: UsuariosComponent, canActivate: [TokenGuard] },
+  { path: 'deportes', component: DeportesListComponent, canActivate: [TokenGuard] },
+  { path: 'deportes/agregar', component: DeporteComponent, canActivate: [TokenGuard] },
+  { path: 'deportes/:nombre', component: DeporteComponent, canActivate: [TokenGuard] },
   { path: '**', component: PageNotFoundComponent }
+
 ];
 
 @NgModule({
@@ -32,6 +44,11 @@ const appRoutes: Routes = [
     PageNotFoundComponent,
     MenuBarComponent,
     NavegacionComponent,
+    UsuariosListComponent,
+    UsuariosComponent,
+    DeportesListComponent,
+    DeporteComponent,
+    YesNoPipe
   ],
   imports: [
     RouterModule.forRoot(
@@ -48,6 +65,7 @@ const appRoutes: Routes = [
     TokenGuard,
     SesionService,
     UsuarioService,
+    DeporteService,
     BaseApiService
   ],
   bootstrap: [AppComponent]
