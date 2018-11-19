9eb7bdc9736534ad0e8e08e918c177aeea8fb3fd
Andres Correa <andresco@oktana.io>
2018-11-19 15:26:34 -0300

    Se modifica la IP de los enviornments

--- Frontend/MisMarcadores/src/environments/environment.prod.ts
+++ Frontend/MisMarcadores/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:50426/api'
+  apiUrl: 'http://192.168.7.153:81/api'
 };
--- Frontend/MisMarcadores/src/environments/environment.ts
+++ Frontend/MisMarcadores/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:50426/api'
+  apiUrl: 'http://192.168.7.153:81/api'
 };
 
 /*
