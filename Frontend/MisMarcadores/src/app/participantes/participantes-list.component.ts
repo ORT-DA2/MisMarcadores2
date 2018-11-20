802ae7d897b0d2acd42ddc142a36b735f05134b6
Andres Correa <andresco@oktana.io>
2018-11-20 17:55:31 -0300

    Se crean los componentes para los participantes

---
+++ Frontend/MisMarcadores/src/app/participantes/participantes-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Participante } from '../clases/participante';
+import { ParticipanteService } from '../servicios/participante.service';
+
+@Component({
+    templateUrl: './participantes-list.component.html',
+    styleUrls: ['./participantes-list.css']
+})
+export class ParticipantesListComponent implements OnInit {
+
+    participantes: Array<Participante>;
+    loading = false;
+
+    constructor(private router: Router, private _participantesService: ParticipanteService) {
+    }
+
+    ngOnInit(): void {
+        this.loading = true;
+        this.obtenerParticipantes();
+    }
+
+    private result(data: Array<Participante>): void {
+        this.participantes = data;
+        console.log(this.participantes);
+        this.loading = false;
+    }
+
+    obtenerParticipantes() {
+        this._participantesService.obtenerParticipantes()
+            .subscribe(((data: Array<Participante>) => this.result(data)),
+                ((error: any) => console.log(error))
+            );
+    }
+
+    agregarParticipante() {
+        this.router.navigate(['/participantes/agregar']);
+    }
+
+    editarParticipante(id: string) {
+        this.router.navigate(['/participantes', id]);
+    }
+
+    borrarParticipante(id: string) {
+        this._participantesService.borrarParticipante(id).subscribe(
+            ((data: any) => this.obtenerParticipantes()),
+            ((error: any) => console.log(error))
+        );
+    }
+}
---
+++ Frontend/MisMarcadores/src/app/participantes/participantes.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Participante } from '../clases/participante';
+import { ParticipanteRequest } from '../clases/participante-request';
+import { ParticipanteService } from '../servicios/participante.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from '../core/notification.service';
+
+@Component({
+    templateUrl: './participantes.component.html',
+    styleUrls: ['./participantes.css']
+})
+
+export class ParticipantesComponent implements OnInit {
+    pageTitle = 'Nuevo participante';
+    model: any = {};
+    participante: Participante;
+    participanteRequest: ParticipanteRequest;
+    selectedFile: File;
+    foto: string;
+    isEditing: boolean;
+    idActual: string;
+    btnText = 'Agregar';
+
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private _participantesService: ParticipanteService,
+        private notificationService: NotificationService) { }
+
+    ngOnInit(): void {
+        this.idActual = this.route.snapshot.params['idParticipante'];
+        if (this.idActual != null) {
+            this.pageTitle = 'Editar participante';
+            this.isEditing = true;
+            this.btnText = 'Actualizar';
+            this.obtenerDatosParticipante(this.idActual);
+        }
+    }
+
+    onFileChanged(event) {
+        this.selectedFile = event.target.files[0];
+        const myReader: FileReader = new FileReader();
+        myReader.onloadend = (e) => {
+            this.foto = myReader.result;
+        };
+        myReader.readAsDataURL(this.selectedFile);
+    }
+
+    obtenerDatosParticipante(id: string) {
+        this._participantesService.obtenerParticipante(id)
+            .subscribe((obtainedParticipante: Participante) => {
+                this.participante = obtainedParticipante;
+                this.setearModelo();
+            },
+            error => {
+                this.handleError(error);
+            }
+        );
+    }
+
+    submitForm() {
+        this.setearParticipante();
+        if (this.isEditing) {
+            this.editarParticipante();
+        } else {
+            this.crearParticipante();
+        }
+    }
+
+    crearParticipante() {
+        this._participantesService.agregarParticipante(this.participanteRequest)
+            .subscribe(
+                data => {
+                    this.router.navigate(['/participantes']);
+                },
+                error => {
+                    this.handleError(error);
+                }
+            );
+    }
+
+    editarParticipante() {
+        this._participantesService.editarParticipante(this.idActual, this.participanteRequest)
+            .subscribe(
+                data => {
+                    this.router.navigate(['/participantes']);
+                },
+                error => {
+                    this.handleError(error);
+                }
+            );
+    }
+
+    handleError(error: HttpErrorResponse): void {
+        this.notificationService.display({ message: error.error, severity: 'error' });
+      }
+
+    setearParticipante() {
+        this.participanteRequest = new ParticipanteRequest();
+        this.participanteRequest.nombre = this.model.nombre;
+        this.participanteRequest.foto = this.foto;
+        this.participanteRequest.nombreDeporte = this.model.deporte;
+        this.participanteRequest.esEquipo = this.model.equipo;
+    }
+
+    setearModelo() {
+        this.model.nombre = this.participante.nombre;
+        this.model.foto = this.participante.foto;
+        this.model.deporte = this.participante.deporte.nombre;
+        this.model.equipo = this.participante.esEquipo;
+    }
+}
